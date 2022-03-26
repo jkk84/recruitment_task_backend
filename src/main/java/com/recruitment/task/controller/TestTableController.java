b8f2637a3fe97f363328ded90b48bbccdc99eb31
jkk84 <jarekkloc84@gmail.com>
2022-03-26 17:19:44 +0100

    add api to url

--- src/main/java/com/recruitment/task/controller/TestTableController.java
+++ src/main/java/com/recruitment/task/controller/TestTableController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test-table")
+@RequestMapping("/api/test-table")
 public class TestTableController {
 
     @Autowired
