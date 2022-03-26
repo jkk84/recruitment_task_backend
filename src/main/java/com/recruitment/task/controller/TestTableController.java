0e4ade5f96454c3c8650a47552aef652de0380b6
jkk84 <jarekkloc84@gmail.com>
2022-03-26 15:56:06 +0100

    query for unique and duplicate lists

--- src/main/java/com/recruitment/task/controller/TestTableController.java
+++ src/main/java/com/recruitment/task/controller/TestTableController.java
@@ -1,13 +1,11 @@
 package com.recruitment.task.controller;
 
-import com.recruitment.task.model.TestTable;
 import com.recruitment.task.service.TestTableService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/test-table")
@@ -16,8 +14,8 @@ public class TestTableController {
     @Autowired
     private TestTableService testTableService;
 
-    @GetMapping
-    public List<TestTable> getTestTableList() {
-        return testTableService.getTestTableList();
+    @GetMapping("/get-lists")
+    public TestTableService.TestTableLists getLists(@RequestParam String columnName) {
+        return testTableService.getLists(columnName);
     }
 }
--- src/main/java/com/recruitment/task/service/TestTableService.java
+++ src/main/java/com/recruitment/task/service/TestTableService.java
@@ -1,19 +1,43 @@
 package com.recruitment.task.service;
 
 import com.recruitment.task.model.TestTable;
-import com.recruitment.task.repository.TestTableRepository;
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TestTableService {
 
     @Autowired
-    private TestTableRepository testTableRepository;
+    EntityManager entityManager;
 
-    public List<TestTable> getTestTableList() {
-        return testTableRepository.findAll();
+    public TestTableLists getLists(String s) {
+
+        boolean contains = Arrays.asList("kolumna1", "kolumna2", "kolumna3", "kolumna4").contains(s);
+
+        String query = "SELECT * FROM tabela_testowa as t " +
+                "JOIN (SELECT %s, COUNT(*) FROM tabela_testowa GROUP BY %s HAVING count(*) %s 1 ) as b " +
+                "ON t.%s = b.%s ORDER BY t.id asc";
+
+        return TestTableLists
+                .builder()
+                .duplicates(!contains ? new ArrayList<>() :
+                        entityManager.createNativeQuery(String.format(query, s, s, ">", s, s), TestTable.class).getResultList())
+                .uniques(!contains ? new ArrayList<>() :
+                        entityManager.createNativeQuery(String.format(query, s, s, "=", s, s), TestTable.class).getResultList())
+                .build();
+    }
+
+    @Data
+    @Builder
+    public static class TestTableLists {
+        private List<TestTable> duplicates;
+        private List<TestTable> uniques;
     }
 }
